6ca924d5716c7f0c909794b852065a190cffd017
benlgw <ben.greaveswarwick@tutamail.com>
2026-06-22 20:41:06 +0100

    fixed a bug which returned nothing if temp and fellsLike were the same

--- src/assets/modules/temp.js
+++ src/assets/modules/temp.js
@@ -7,47 +7,52 @@ class Temp {
 	getCardText() {
 		// Freezing
 		if (this.temperature <= 0) {
-			return `The Stick is Frozen Solid. It's ${this.temperature}` +
-				this.temperature ===
-				this.feelsLike
-				? ` but feels like ${this.feelsLike}`
-				: "";
+			return (
+				`The Stick is Frozen Solid. It's ${this.temperature}` +
+				(this.temperature === this.feelsLike
+					? ""
+					: ` but feels like ${this.feelsLike}`)
+			);
 		}
 
 		// Cold
 		if (this.temperature <= 10) {
-			return `The Stick is Cold. It's ${this.temperature}` +
-				this.temperature ===
-				this.feelsLike
-				? ` but feels like ${this.feelsLike}`
-				: "";
+			return (
+				`The Stick is Cold. It's ${this.temperature}` +
+				(this.temperature === this.feelsLike
+					? ""
+					: ` but feels like ${this.feelsLike}`)
+			);
 		}
 
 		// Cool
 		if (this.temperature <= 15) {
-			return `The Stick is Comfertable. It's ${this.temperature}` +
-				this.temperature ===
-				this.feelsLike
-				? ` but feels like ${this.feelsLike}`
-				: "";
+			return (
+				`The Stick is Comfertable. It's ${this.temperature}` +
+				(this.temperature === this.feelsLike
+					? ""
+					: ` but feels like ${this.feelsLike}`)
+			);
 		}
 
 		// Warm
 		if (this.temperature <= 20) {
-			return `The Stick is Warm. It's ${this.temperature}` +
-				this.temperature ===
-				this.feelsLike
-				? ` but feels like ${this.feelsLike}`
-				: "";
+			return (
+				`The Stick is Warm. It's ${this.temperature}` +
+				(this.temperature === this.feelsLike
+					? ""
+					: ` but feels like ${this.feelsLike}`)
+			);
 		}
 
 		// Hot
 		if (this.temperature > 20) {
-			return `The Stick is Sweating. It's ${this.temperature}` +
-				this.temperature ===
-				this.feelsLike
-				? ` but feels like ${this.feelsLike}`
-				: "";
+			return (
+				`The Stick is Sweating. It's ${this.temperature}` +
+				(this.temperature === this.feelsLike
+					? ""
+					: ` but feels like ${this.feelsLike}`)
+			);
 		}
 	}
 }
