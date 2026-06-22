3bd567505feb4b5bfb33e92416085f6edc33c0d4
benlgw <ben.greaveswarwick@tutamail.com>
2026-06-22 20:29:45 +0100

    use the three modules for handling weather text

--- src/main.js
+++ src/main.js
@@ -1,9 +1,17 @@
 import "./reset.css";
 import "./style.css";
 
+import Condition from "./assets/modules/condition";
+import Temp from "./assets/modules/temp";
+import Wind from "./assets/modules/wind";
+
 const API_KEY = "GRZT3UE6Q5K6D9UC6FSWX8RAR";
 const URL =
 	"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/[LOCATION_HERE]?unitGroup=uk&include=current&key=[API_KEY_HERE]&contentType=json";
+
+const conditionCard = document.querySelector("#condition");
+const tempCard = document.querySelector("#temp");
+const windCard = document.querySelector("#wind");
 
 async function getLocation() {
 	return new Promise((resolve) => {
@@ -30,26 +38,49 @@ async function getWeather(location) {
 	return weatherData;
 }
 
-function createWeatherObject(weatherData) {
-	const currentConidtions = weatherData.currentConditions;
+async function createWeatherObject(weatherData) {
+	const currentConditions = weatherData.currentConditions;
 	return {
 		location: weatherData.address,
-		temperature: currentConidtions.temp,
-		feelsLike: currentConidtions.feelslike,
-		coniditions: currentConidtions.conditions,
-		coniditionsIcon: currentConidtions.icon,
-		windSpeed: currentConidtions.windspeed,
-		windGusts: currentConidtions.windgust,
-		windDirection: currentConidtions.winddir,
+		temperature: currentConditions.temp,
+		feelsLike: currentConditions.feelslike,
+		condition: currentConditions.conditions,
+		conditionIcon: currentConditions.icon,
+		windSpeed: currentConditions.windspeed,
+		windGusts: currentConditions.windgust,
+		windDirection: currentConditions.winddir,
 	};
+}
+
+async function createCardContent(weatherDataObject) {
+	conditionCard.textContent = "";
+	const condition = new Condition(weatherDataObject.condition);
+	const conditionCardText = condition.getCardText();
+	conditionCard.textContent = conditionCardText;
+
+	tempCard.textContent = "";
+	const temp = new Temp(
+		weatherDataObject.temperature,
+		weatherDataObject.feelsLike,
+	);
+	const tempCardText = temp.getCardText();
+	tempCard.textContent = tempCardText;
+
+	windCard.textContent = "";
+	const wind = new Wind(
+		weatherDataObject.windSpeed,
+		weatherDataObject.windGusts,
+		weatherDataObject.windDirection,
+	);
+	const windCardText = wind.getCardText();
+	windCard.textContent = windCardText;
 }
 
 async function main() {
 	const location = await getLocation();
-	console.log(location);
 	const weatherData = await getWeather(location);
 	const weatherDataObject = await createWeatherObject(weatherData);
-	console.log(weatherDataObject);
+	await createCardContent(weatherDataObject);
 }
 
 main();
