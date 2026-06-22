fd85f7de3248a705a82f05e60bfacddffc8f0cb1
benlgw <ben.greaveswarwick@tutamail.com>
2026-06-22 20:43:38 +0100

    added degree C sumbol to temperature text

--- src/assets/modules/temp.js
+++ src/assets/modules/temp.js
@@ -8,50 +8,50 @@ class Temp {
 		// Freezing
 		if (this.temperature <= 0) {
 			return (
-				`The Stick is Frozen Solid. It's ${this.temperature}` +
+				`The Stick is Frozen Solid. It's ${this.temperature}°C` +
 				(this.temperature === this.feelsLike
 					? ""
-					: ` but feels like ${this.feelsLike}`)
+					: ` but feels like ${this.feelsLike}°C`)
 			);
 		}
 
 		// Cold
 		if (this.temperature <= 10) {
 			return (
-				`The Stick is Cold. It's ${this.temperature}` +
+				`The Stick is Cold. It's ${this.temperature}°C` +
 				(this.temperature === this.feelsLike
 					? ""
-					: ` but feels like ${this.feelsLike}`)
+					: ` but feels like ${this.feelsLike}°C`)
 			);
 		}
 
 		// Cool
 		if (this.temperature <= 15) {
 			return (
-				`The Stick is Comfertable. It's ${this.temperature}` +
+				`The Stick is Comfertable. It's ${this.temperature}°C` +
 				(this.temperature === this.feelsLike
 					? ""
-					: ` but feels like ${this.feelsLike}`)
+					: ` but feels like ${this.feelsLike}°C`)
 			);
 		}
 
 		// Warm
 		if (this.temperature <= 20) {
 			return (
-				`The Stick is Warm. It's ${this.temperature}` +
+				`The Stick is Warm. It's ${this.temperature}°C` +
 				(this.temperature === this.feelsLike
 					? ""
-					: ` but feels like ${this.feelsLike}`)
+					: ` but feels like ${this.feelsLike}°C`)
 			);
 		}
 
 		// Hot
 		if (this.temperature > 20) {
 			return (
-				`The Stick is Sweating. It's ${this.temperature}` +
+				`The Stick is Sweating. It's ${this.temperature}°C` +
 				(this.temperature === this.feelsLike
 					? ""
-					: ` but feels like ${this.feelsLike}`)
+					: ` but feels like ${this.feelsLike}°C`)
 			);
 		}
 	}
