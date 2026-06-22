26d204f4055cdba80d71aa7ea2ee23e916f41413
benlgw <ben.greaveswarwick@tutamail.com>
2026-06-22 13:42:55 +0100

    create function to get weather data based on location

--- src/main.js
+++ src/main.js
@@ -1,2 +1,16 @@
 import "./reset.css";
 import "./style.css";
+
+const API_KEY = "GRZT3UE6Q5K6D9UC6FSWX8RAR";
+const URL =
+	"https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/<LOCATION_HERE>?unitGroup=uk&include=current&key=<API_KEY_HERE>&contentType=json";
+
+async function getWeather(location) {
+	const url = URL.split("<LOCATION_HERE>")
+		.join(location)
+		.split("<API_KEY_HERE>")
+		.join(API_KEY);
+	const response = await fetch(url);
+	const weatherData = await response.json();
+	return weatherData;
+}
